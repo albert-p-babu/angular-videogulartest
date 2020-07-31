fb36cb327e7679d43f3b1a010310e0000b96f841
albert-p-babu <albertpallissery@gmail.com>
2020-07-31 16:03:08 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-videogulartest
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-videogulartest)
---
+++ app/hello.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'uplife',
+  template: `<h1>{{name}}!</h1>
+      <vg-player>
+        <video [vgMedia]="media" #media id="singleVideo" preload="auto" controls>
+            <source src="https://firebasestorage.googleapis.com/v0/b/uplife-83c06.appspot.com/o/videoplayback.mp4?alt=media&token=e227d222-78bb-4e53-9051-c29878509238" type="video/mp4">
+        </video>
+    </vg-player>
+`,
+  styles: [`h1 { font-family: Lato; }`]
+})
+export class HelloComponent  {
+  @Input() name: string;
+}
