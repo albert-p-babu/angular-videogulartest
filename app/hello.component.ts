5f5ce7a741f84350d4a2efd7a5d00efddaaf8a08
albert-p-babu <albertpallissery@gmail.com>
2020-07-31 16:26:19 +0530

    1 file modified

--- app/hello.component.ts
+++ app/hello.component.ts
@@ -4,7 +4,7 @@ import { Component, Input } from '@angular/core';
   selector: 'uplife',
   template: `<h1>{{name}}</h1>
       <vg-player>
-        <video [vgMedia]="media" #media id="singleVideo" preload="auto" controls>
+        <video [vgMedia]="media" #media id="singleVideo" preload="auto" controls width="75%" >
             <source src="https://firebasestorage.googleapis.com/v0/b/uplife-83c06.appspot.com/o/videoplayback.mp4?alt=media&token=e227d222-78bb-4e53-9051-c29878509238" type="video/mp4">
         </video>
     </vg-player>
